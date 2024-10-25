0e42290cc5b0526b1a96b1e21212043a4cf3c3fc
RemiL83 <reloite@gmail.com>
2024-10-25 23:34:54 +0200

    Add files via upload

---
+++ dictionary.mjs
@@ -0,0 +1,18 @@
+const GAME_DICTIONARY = {
+    player1: "Du vant!",
+    player2: "Datamaskinen vant.",
+    draw: "Det ble uavgjort",
+    winner: "Vinneren er : ",
+    selectionQuestion: "Velg mellom (S)tein, S(a)ks, (P)apir",
+    youPicked: "Du valgte",
+    aiPicked: "Datamaskinen valgte",
+    rock: "S",
+    paper: "P",
+    scissors: "A",
+    choices: ["Stein", "Papir", "Saks"],
+    title: "La oss spille stein saks papir",
+    startGame: "Trykk Enter for å starte.",
+    replay: "Vil du spille igjen? J/N"
+};
+
+export default GAME_DICTIONARY;
